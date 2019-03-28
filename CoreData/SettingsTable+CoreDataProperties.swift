28d0c1e874000b3afc98c9534b67ea5c6ef6bd74
losevfa <losevfa2711@gmail.com>
2019-03-28 20:59:51 +0700

    update DB

--- CoreData/SettingsTable+CoreDataProperties.swift
+++ CoreData/SettingsTable+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  SettingsTable+CoreDataProperties.swift
 //  myWeather
 //
-//  Created by admin on 26/03/2019.
+//  Created by admin on 28/03/2019.
 //  Copyright © 2019 admin. All rights reserved.
 //
 //
@@ -19,5 +19,9 @@ extension SettingsTable {
 
     @NSManaged public var selectTemp: Int16
     @NSManaged public var temperature: String?
+    @NSManaged public var selectPress: Int16
+    @NSManaged public var pressure: String?
+    @NSManaged public var windSpeed: String?
+    @NSManaged public var selectWindSpeed: Int16
 
 }
